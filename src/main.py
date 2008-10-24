fe564eae8ca6e937f57bcdfdcaf30d269e3edf55
João Magalhães <joamag@hive.pt>
2008-10-24 16:34:32 +0000

    new project inclusion
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@150 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/main.py
+++ src/main.py
@@ -152,6 +152,7 @@ def main():
     prefix_path + "pt.hive.colony.plugins.adapters.output/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.adapters.output.handlers/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.build.automation/src/colony/plugins;" +\
+    prefix_path + "pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.business/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.configuration.startup/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.dummy/src/colony/plugins;" +\
