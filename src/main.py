7b9f6e6fc0d716ac54ad2c606260d58438e37ac7
João Magalhães <joamag@hive.pt>
2009-01-20 11:53:04 +0000

    support for access control plugin
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@1031 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/main.py
+++ src/main.py
@@ -165,6 +165,7 @@ def main():
     prefix_path + "pt.hive.colony.plugins.build.automation.extensions/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.build.automation.items/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.business/src/colony/plugins;" +\
+    prefix_path + "pt.hive.colony.plugins.business.access_control/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.document/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.configuration.startup/src/colony/plugins;" +\
     prefix_path + "pt.hive.colony.plugins.distribution/src/colony/plugins;" +\
