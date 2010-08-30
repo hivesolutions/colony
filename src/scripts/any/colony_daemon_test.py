b7a678ee130b5b5fb1e8adec636880c23e5f67c1
João Magalhães <joamag@hive.pt>
2010-08-30 10:34:22 +0000

    minor script code change
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@9913 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/scripts/any/colony_daemon_test.py
+++ src/scripts/any/colony_daemon_test.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = "João Magalhães <joamag@hive.pt> & Tiago Silva <tsilva@hive.pt>"
+__author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
 __version__ = "1.0.0"
