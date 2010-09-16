7b324462bd45a0d5ce01459cfe227d9df6794d9d
João Magalhães <joamag@hive.pt>
2010-09-16 10:54:30 +0000

    minor code refactoring
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@10443 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/scripts/win32/colony_script.bat
+++ src/scripts/win32/colony_script.bat
@@ -28,7 +28,7 @@
 
 :: sets the temporary variables
 set RELATIVE_PATH=../../
-set SCRIPT_NAME=main.py
+set SCRIPT_NAME=script.py
 
 :: executes the initial python script with
 :: the provided arguments
