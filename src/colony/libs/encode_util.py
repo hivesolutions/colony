c982e7cbb047d3c72e1034404a4492a9732fe88c
João Magalhães <joamag@hive.pt>
2010-10-06 16:43:23 +0000

    better encode utils support
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@10860 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/colony/libs/encode_util.py
+++ src/colony/libs/encode_util.py
@@ -39,7 +39,7 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import binascii
 
-def encode_long(long_value):
+def encode_two_complement_string(long_value):
     """
     Encode a long to a two's complement little-endian binary string.
     Note that 0L is a special case, returning an empty string, to save a
@@ -61,61 +61,52 @@ def encode_long(long_value):
         # converts the long value to the hexadecimal string value
         long_value_hexadecial = hex(long_value)
 
-        # asserts that the hexadecimal starts with the hexadecimal
-        # initialization value
-        assert long_value_hexadecial.startswith("0x")
+        # counts the number of nibbles in the given value
+        number_nibbles = _count_nibbles(long_value_hexadecial)
 
-        # calculates the number of "junk" characters
-        number_junk_characters = 2 + long_value_hexadecial.endswith("L")
-
-        # calculates the nibles from the length of the long value string
-        # without the junk characters
-        nibbles = len(long_value_hexadecial) - number_junk_characters
-
-        if nibbles & 1:
+        # in case the number of nibbles is odd
+        if number_nibbles & 1:
             # need an even # of nibbles for unhexlify
             long_value_hexadecial = "0x0" + long_value_hexadecial[2:]
+        # in case the number i
         elif int(long_value_hexadecial[2], 16) >= 8:
             # looks negative so need a byte of sign bits
             long_value_hexadecial = "0x00" + long_value_hexadecial[2:]
     else:
-        # Build the 256's-complement:  (1L << nbytes) + x.  The trick is
-        # to find the number of bytes in linear time (although that should
-        # really be a constant-time task)
+        # converts the (negative) long value to the hexadecimal string value
         long_value_hexadecial = hex(-long_value)
 
-        # asserts that the hexadecimal starts with the hexadecimal
-        # initialization value
-        assert long_value_hexadecial.startswith("0x")
+        # counts the number of nibbles in the given value
+        number_nibbles = _count_nibbles(long_value_hexadecial)
 
-        # calculates the nibles from the length of the long value string
-        # without the junk characters
-        number_junk_characters = 2 + long_value_hexadecial.endswith("L")
-
-        nibbles = len(long_value_hexadecial) - number_junk_characters
-        if nibbles & 1:
+        # in case the number of nibbles is odd
+        if number_nibbles & 1:
             # extends to a full byte.
-            nibbles += 1
+            number_nibbles += 1
 
-        # calculates the number of bits from the nibles
-        number_bits = nibbles * 4
+        # calculates the number of bits from the nibbles
+        number_bits = number_nibbles * 4
 
         # puts the negative indication as the last digit
         long_value += 1L << number_bits
 
-        # asserts that the long value is non negative
-        assert long_value > 0
-
+        # converts the long value to the hexadecimal string value
         long_value_hexadecial = hex(long_value)
-        njunkchars = 2 + long_value_hexadecial.endswith('L')
-        newnibbles = len(long_value_hexadecial) - njunkchars
-        if newnibbles < nibbles:
-            long_value_hexadecial = "0x" + "0" * (nibbles - newnibbles) + long_value_hexadecial[2:]
+
+        # counts the number of nibbles in the given value
+        new_number_nibbles = _count_nibbles(long_value_hexadecial)
+
+        # in case the new number of nibbles is smaller than
+        # the previous one
+        if new_number_nibbles < number_nibbles:
+            # puts the sign bits
+            long_value_hexadecial = "0x" + "0" * (number_nibbles - new_number_nibbles) + long_value_hexadecial[2:]
+        # in case it's a positive number
         if int(long_value_hexadecial[2], 16) < 8:
-            # "looks positive", so need a byte of sign bits
+            # no need a byte of sign bits
             long_value_hexadecial = "0xff" + long_value_hexadecial[2:]
 
-    # in case the long value hexadeciaml ends with the long
+    # in case the long value hexadecimal ends with the long
     # indication value
     if long_value_hexadecial.endswith("L"):
         # removes the last character to avoid the extra long indication
@@ -125,8 +116,7 @@ def encode_long(long_value):
         # sets the normal value without the "0x" initialization
         long_value_hexadecial = long_value_hexadecial[2:]
 
-    assert len(long_value_hexadecial) & 1 == 0, (long_value, long_value_hexadecial)
-
+    # unhexflies the value retrieving the binary value
     binary = binascii.unhexlify(long_value_hexadecial)
 
     # reverses the binary value
@@ -135,7 +125,7 @@ def encode_long(long_value):
     # returns the reversed binary value
     return reversed_binary
 
-def decode_long(data):
+def decode_two_complement_string(data):
     """
     Decode a long from a two's complement little-endian binary string.
 
@@ -167,3 +157,25 @@ def decode_long(data):
 
     # returns the long value
     return long_value
+
+def _count_nibbles(long_value_hexadecial):
+    """
+    Calculates the number of nibbles (4 bit group) from the given
+    long value encoded in hexadecimal string.
+
+    @type long_value_hexadecial: String
+    @param long_value_hexadecial: The long value encoded in
+    hexadecimal string to count the nibbles.
+    @rtype: int
+    @return: The number of nibbles in the given value.
+    """
+
+    # calculates the number of "junk" characters
+    number_junk_characters = 2 + long_value_hexadecial.endswith("L")
+
+    # calculates the number of nibbles from the length of the
+    # long value string without the junk characters
+    number_nibbles = len(long_value_hexadecial) - number_junk_characters
+
+    # returns the number of nibbles
+    return number_nibbles
