3fd0e1869723f10a703182fda9431bd42066581b
João Magalhães <joamag@hive.pt>
2011-04-25 15:14:48 +0000

    new code change
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@13514 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/scripts/unix/colony.sh
+++ src/scripts/unix/colony.sh
@@ -27,13 +27,15 @@
 # __license__   = GNU General Public License (GPL), Version 3
 
 # sets the temporary variables
-BIN_PATH=/usr/bin
-PYTHON_PATH=$BIN_PATH/python
+BIN_PATH=/bin
+USR_BIN_PATH=/usr/bin
+SHELL_PATH=$BIN_PATH/sh
+PYTHON_PATH=$USR_BIN_PATH/python
 RELATIVE_PATH=../../
 SCRIPT_NAME=main.py
 
 # retrieves the current directory
-CURRENT_DIRECTORY=sh $(dirname $0)/realpath.sh $0
+CURRENT_DIRECTORY=$($SHELL_PATH $(dirname $0)/realpath.sh $0)
 
 # executes the initial python script with
 # the provided arguments
