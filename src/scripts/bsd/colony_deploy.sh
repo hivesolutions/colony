9a36575067579e417ebfde4c3869716460fac4e9
João Magalhães <joamag@hive.pt>
2011-04-25 16:37:36 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@13526 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/scripts/bsd/colony_deploy.sh
+++ src/scripts/bsd/colony_deploy.sh
@@ -33,7 +33,7 @@ RELATIVE_PATH=../all/
 SCRIPT_NAME=colony_deploy.py
 
 # retrieves the script directory path
-SCRIPT_DIRECTORY_PATH=$PYTHON_PATH -c "import os;print os.path.realpath(\"$0\")"
+SCRIPT_DIRECTORY_PATH=$($PYTHON_PATH -c "import os;print os.path.realpath(\"$0\")")
 
 # executes the initial python script with
 # the provided arguments
