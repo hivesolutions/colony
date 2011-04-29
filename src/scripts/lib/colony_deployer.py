d49001630af98a47fbadbbc5af173ff377605024
João Magalhães <joamag@hive.pt>
2011-04-29 14:29:38 +0000

    minor code refactor
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@13756 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/scripts/lib/colony_deployer.py
+++ src/scripts/lib/colony_deployer.py
@@ -268,7 +268,7 @@ class Deployer:
             colony_file.remove_directory(temporary_path)
 
         # prints a log message
-        self.log("Finished deploying '%s'" % package_path, logging.INFO)
+        self.log("Finished deploying '%s' to '%s'" % (package_path, self.manager_path), logging.INFO)
 
     def deploy_bundle_package(self, package_path, temporary_path):
         """
