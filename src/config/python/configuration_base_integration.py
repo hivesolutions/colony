d0a6faf06b431329da7193f8f92186998369b0e7
João Magalhães <joamag@hive.pt>
2011-08-11 15:52:23 +0000

    new facebook site reference
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@16229 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/config/python/configuration_base_integration.py
+++ src/config/python/configuration_base_integration.py
@@ -200,6 +200,7 @@ plugin_path_list = [
     "%hive_development_prefix_path%/pt.hive.hive_development.plugins.web_dropbox/src/hive_development/plugins",
     "%hive_development_prefix_path%/pt.hive.hive_development.plugins.web_toolbar/src/hive_development/plugins",
     "%products_prefix_path%/pt.hive.products.plugins.build.automation.items/src/products/plugins",
+    "%bargania_prefix_path%/com.bargania.bargania_site.plugins.facebook/src/bargania_site/plugins",
     "%bargania_prefix_path%/com.bargania.bargania_site.plugins.main/src/bargania_site/plugins"
 ]
 """ The list of plugin paths """
