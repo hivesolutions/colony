f0548dfa7a59d1fe99b85714c556123589e1eeab
João Magalhães <joamag@hive.pt>
2011-10-07 17:53:37 +0000

    reverted minor update
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@16917 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/colony/base/plugin_system.py
+++ src/colony/base/plugin_system.py
@@ -22,7 +22,7 @@
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
-__version__ = "1.0.1"
+__version__ = "1.0.0"
 """ The version of the module """
 
 __revision__ = "$LastChangedRevision: 9958 $"
