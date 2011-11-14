642547fbc338a8d21fd29d9037e90d67906365ab
João Magalhães <joamag@hive.pt>
2011-11-14 11:22:40 +0000

    new configuration file
    git-svn-id: http://svn.hive.pt/hive-main/pt.hive.colony/trunk/pt.hive.colony@17776 89aa9a68-8201-4d56-8cc5-2cc230aca7a0

--- src/config/python/configuration_base_integration.py
+++ src/config/python/configuration_base_integration.py
@@ -197,6 +197,8 @@ plugin_path_list = [
     "%omni_prefix_path%/pt.hive.omni.plugins.web_sam/src/omni/plugins",
     "%omni_prefix_path%/pt.hive.omni.plugins.web_srm/src/omni/plugins",
     "%omni_data_prefix_path%/pt.hive.omni.data.plugins.development/src/omni/plugins",
+    "%hive_site_prefix_path%/pt.hive.hive_site.plugins.main/src/hive_site/plugins",
+    "%hive_blog_prefix_path%/pt.hive.hive_blog.plugins.main/src/hive_blog/plugins",
     "%hive_development_prefix_path%/pt.hive.hive_development.plugins.bargania_analyzer/src/hive_development/plugins",
     "%hive_development_prefix_path%/pt.hive.hive_development.plugins.library_repository/src/hive_development/plugins",
     "%hive_development_prefix_path%/pt.hive.hive_development.plugins.media_dashboard/src/hive_development/plugins",
