e76abb9d216d4980d49c890b9d0f402d0c13ccb3
João Magalhães <joamag@gmail.com>
2012-06-25 17:26:06 +0100

    small code refactor

--- scripts/pypi/colony_pypi.py
+++ scripts/pypi/colony_pypi.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     path = os.path.dirname(main.__file__)
     _path = os.getcwd()
     try:
-        sys.path.insert(0, _path)
+        sys.path.insert(0, path)
         os.chdir(path)
         main.main()
     finally:
