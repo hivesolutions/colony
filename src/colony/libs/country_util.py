26849d8dd39b32171b6fe9ff80c23f33abf082ac
Tiago Silva <eng.tiago.silva@gmail.com>
2012-11-16 17:07:30 +0000

    small code standard fix in country util

--- src/colony/libs/country_util.py
+++ src/colony/libs/country_util.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-COUNTRIES  = {
+COUNTRIES = {
     "aaland islands" : ("AX", "ALA", "248"),
     "afghanistan" : ("AF", "AFG", "004"),
     "albania" : ("AL", "ALB", "008"),
