18021b577b0bd3087bb9ef1b0bfdc8aac1943e9d
joamag <joamag@gmail.com>
2013-02-05 20:01:17 +0000

    new version file value

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ class ProcessCommand(setuptools.Command):
 
 setuptools.setup(
     name = "colony",
-    version = "1.0.5",
+    version = "1.0.6",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Colony Framework",
