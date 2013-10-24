1ad800fbd6c89233d1b3523746798b3c268fd910
joamag <joamag@gmail.com>
2013-10-24 19:01:55 +0100

    new admin tool naming

--- src/colony/base/system.py
+++ src/colony/base/system.py
@@ -22,7 +22,7 @@
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 """ The version of the module """
 
 __revision__ = "$LastChangedRevision$"
