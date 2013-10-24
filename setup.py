8a1308f8c36a5042aca1d2f11839dba0d43540bb
joamag <joamag@gmail.com>
2013-10-24 19:02:53 +0100

    new file support

--- setup.py
+++ setup.py
@@ -230,9 +230,9 @@ setuptools.setup(
     zip_safe = False,
     scripts = [
         "scripts/pypi/colony.bat",
-        "scripts/pypi/colony_admin.py",
+        "scripts/pypi/colony_adm.py",
         "scripts/pypi/colony_pypi.py",
-        "scripts/pypi/colony_admin.bat"
+        "scripts/pypi/colony_adm.bat"
     ],
     py_modules = [
         "colony_adm",
