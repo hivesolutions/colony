b125e7ac06477e80174eea6a17b8326a47e54214
joamag <joamag@gmail.com>
2014-04-24 14:29:30 +0100

    new year

--- scripts/service/colony_wrapper.sh
+++ scripts/service/colony_wrapper.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Hive Colony Framework
-# Copyright (c) 2008-2012 Hive Solutions Lda.
+# Copyright (c) 2008-2014 Hive Solutions Lda.
 #
 # This file is part of Hive Colony Framework.
 #
@@ -23,7 +23,7 @@
 # __version__   = 1.0.0
 # __revision__  = $LastChangedRevision$
 # __date__      = $LastChangedDate$
-# __copyright__ = Copyright (c) 2008-2012 Hive Solutions Lda.
+# __copyright__ = Copyright (c) 2008-2014 Hive Solutions Lda.
 # __license__   = GNU General Public License (GPL), Version 3
 
 # the initial variables
