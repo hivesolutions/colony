0fc38f72022656dc00227d4cdacc35326e16ab0c
joamag <joamag@gmail.com>
2014-06-20 17:15:12 +0100

    support for home file path usage

--- src/colony/base/config.py
+++ src/colony/base/config.py
@@ -107,6 +107,7 @@ def conf_s(name, value):
     CONFIGS[name] = value
 
 def load(path = None):
+    load_file(path = os.path.expanduser("~"))
     load_file(path = util.resolve_manager())
     load_file(path = path)
     load_env()
