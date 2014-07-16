5f3b26418c637d08d3215c82e4e48b1c95f3be90
joamag <joamag@gmail.com>
2014-07-16 16:58:19 +0100

    new version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ import setuptools
 
 setuptools.setup(
     name = "colony",
-    version = "1.2.1",
+    version = "1.2.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Colony Framework",
