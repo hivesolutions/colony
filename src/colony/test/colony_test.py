8d441fa9ba25909be0f0f23ab25ce7c78b97cb15
joamag <joamag@gmail.com>
2014-10-31 20:18:54 +0000

    new code

--- src/colony/test/colony_test.py
+++ src/colony/test/colony_test.py
@@ -37,5 +37,5 @@ __copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-from base import * #@UnusedWildImport
-from libs import * #@UnusedWildImport
+from .base import * #@UnusedWildImport
+from .libs import * #@UnusedWildImport
