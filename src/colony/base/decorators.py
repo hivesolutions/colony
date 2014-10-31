b351d262f594035a10af007aa597c7fa04ebee8b
joamag <joamag@gmail.com>
2014-10-31 19:34:18 +0000

    new system inclusion

--- src/colony/base/decorators.py
+++ src/colony/base/decorators.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import system
+from colony.base import system
 
 METHOD_NAME_VALUE = "method_name"
 """ The method name value """
