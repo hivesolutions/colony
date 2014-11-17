1c09ed434445fa8826f872fb6f9d22842e155930
joamag <joamag@gmail.com>
2014-11-17 12:15:50 +0000

    new version

--- src/colony/base/system.py
+++ src/colony/base/system.py
@@ -22,7 +22,7 @@
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
-__version__ = "1.2.21"
+__version__ = "1.2.22"
 """ The version of the module """
 
 __revision__ = "$LastChangedRevision$"
