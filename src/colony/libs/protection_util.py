2cc7a0785449c35e598f295c515acd58ed2b955a
joamag <joamag@gmail.com>
2014-11-18 16:52:13 +0000

    simplified protection util

--- src/colony/libs/protection_util.py
+++ src/colony/libs/protection_util.py
@@ -66,7 +66,7 @@ class Protected(object):
 
     def __new__(cls, *args, **kwargs):
         # creates a new object reference
-        object_reference = object.__new__(cls, *args, **kwargs)
+        object_reference = object.__new__(cls)
 
         # initializes the class reference with the new
         # object reference and the arguments
